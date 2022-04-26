85eabddad8fc13cdcdf62fa8261011731b891210
moltyhero <matanoded@gmail.com>
2022-04-26 18:26:37 +0300

    Define grid bounderies

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ for (let i = 0; i < 4; i++) {
     let mainDiv = document.createElement('div');
     for (let j = 0; j < 4; j++) {
         let div = document.createElement('div');
+        div.classList.add('grid');
+        div.textContent = 'a';
         mainDiv.appendChild(div);
     }
     container.appendChild(mainDiv);
