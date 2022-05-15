a6c33d389986be66f3573b4c58a60474aa5068e6
moltyhero <matanoded@gmail.com>
2022-05-16 02:13:55 +0300

    Create a function with dynamic grid

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 const container = document.querySelector('.container');
 
-// Create 4x4 grid of divs
-for (let i = 0; i < 4; i++) {
-    let mainDiv = document.createElement('div');
-    for (let j = 0; j < 4; j++) {
-        let div = document.createElement('div');
-        div.classList.add('grid');
-        div.textContent = 'a';
-        mainDiv.appendChild(div);
+function createGrid(verticalSquares, horizontalSquares) {
+    for (let i = 0; i < verticalSquares; i++) {
+        let mainDiv = document.createElement('div');
+        for (let j = 0; j < horizontalSquares; j++) {
+            let div = document.createElement('div');
+            div.classList.add('grid');
+            div.textContent = 'a'; // For testing
+
+            // Set currect div size by total allowed
+            containerWidth = parseInt(getComputedStyle(document.querySelector('.container')).width);
+            containerHeight = parseInt(getComputedStyle(document.querySelector('.container')).height);
+            div.style.width = containerWidth/horizontalSquares + "px";
+            div.style.height = containerHeight/verticalSquares + "px";
+
+            div.addEventListener('mouseover', () => { // Add hover effect to each pixel
+                div.style.backgroundColor = 'yellow';
+            })
+    
+            mainDiv.appendChild(div);
+        }
+        container.appendChild(mainDiv);
     }
-    container.appendChild(mainDiv);
 }
+
+createGrid(16, 16);
+
