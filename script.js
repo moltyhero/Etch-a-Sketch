073718cb6158f9a52ec8cac917e94c08bbdcec45
moltyhero <matanoded@gmail.com>
2022-05-16 04:14:28 +0300

    Format

--- script.js
+++ script.js
@@ -1,41 +1,48 @@
-const container = document.querySelector('.container');
+const container = document.querySelector(".container");
 
 function createGrid(verticalSquares, horizontalSquares) {
-    container.textContent = '';
-    for (let i = 0; i < verticalSquares; i++) {
-        let mainDiv = document.createElement('div');
-        for (let j = 0; j < horizontalSquares; j++) {
-            let div = document.createElement('div');
-            div.classList.add('grid');
+	container.textContent = "";
+	for (let i = 0; i < verticalSquares; i++) {
+		let mainDiv = document.createElement("div");
+		for (let j = 0; j < horizontalSquares; j++) {
+			let div = document.createElement("div");
+			div.classList.add("grid");
 
-            // Set currect div size by total allowed
-            containerWidth = parseInt(getComputedStyle(document.querySelector('.container')).width);
-            containerHeight = parseInt(getComputedStyle(document.querySelector('.container')).height);
-            div.style.width = containerWidth/horizontalSquares + "px";
-            div.style.height = containerHeight/verticalSquares + "px";
+			// Set currect div size by total allowed
+			containerWidth = parseInt(
+				getComputedStyle(document.querySelector(".container")).width
+			);
+			containerHeight = parseInt(
+				getComputedStyle(document.querySelector(".container")).height
+			);
+			div.style.width = containerWidth / horizontalSquares + "px";
+			div.style.height = containerHeight / verticalSquares + "px";
 
-            div.addEventListener('mouseover', () => { // Add hover effect to each pixel
+			div.addEventListener("mouseover", () => {
+				// Add hover effect to each pixel
 
-                div.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)},
+				div.style.backgroundColor = `rgb(
+                    ${Math.floor(Math.random() * 256)},
+                    ${Math.floor(Math.random() * 256)},
                     ${Math.floor(Math.random() * 256)})`;
-                // let currentBrighness = div.style.filter;
-                // console.log(currentBrighness);
-                // div.style.filter = `brightness(${})`;
-            })
-    
-            mainDiv.appendChild(div);
-        }
-        container.appendChild(mainDiv);
-    }
+				// let currentBrighness = div.style.filter;
+				// console.log(currentBrighness);
+				// div.style.filter = `brightness(${})`;
+			});
+
+			mainDiv.appendChild(div);
+		}
+		container.appendChild(mainDiv);
+	}
 }
 
-createGrid(16, 16);
+let gridSizeButton = document.getElementById("gridSizeButton");
+gridSizeButton.addEventListener("click", () => {
+	let size = prompt("Enter size under 100");
+	while (size >= 100) {
+		size = prompt("This is bigger than 100, please enter a lower number");
+	}
+	createGrid(size, size);
+});
 
-let gridSizeButton = document.getElementById('gridSizeButton');
-gridSizeButton.addEventListener('click', () => {
-    let size = prompt("Enter size under 100");
-    while (size>=100) {
-        size = prompt("This is bigger than 100, please enter a lower number");
-    }
-    createGrid(size, size);
-})
+createGrid(16, 16);
